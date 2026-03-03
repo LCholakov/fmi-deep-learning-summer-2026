d6ad812d36f47737520cb08cfdfd80db1845c38b
Lyubomir Cholakov <39132342+LCholakov@users.noreply.github.com>
2026-03-03 21:55:04 +0200

    cleanup and format
    comment the lists prior to format to avoid yapf splitting them into lines

--- week-01/matplotlib/task01.py
+++ week-01/matplotlib/task01.py
@@ -1,16 +1,13 @@
 import matplotlib.pyplot as plt
 
-
-import matplotlib.ticker as mticker
-
 # Q1: Projected population chart shows we could reach 10 billion arond 2062
 
 # Q2: Analysis of life expect to GDP/cap chart:
-# Clear correlation shows higher GDP/cap is a decent predictor of longer life expectation. 
-# Although easy to assume higher GDP = better health services, 
+# Clear correlation shows higher GDP/cap is a decent predictor of longer life expectation.
+# Although easy to assume higher GDP = better health services,
 # we need more data to claim any causation between the stats.
 
-# Draw both in subplots 
+# Draw both in subplots
 
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
 fig.set_facecolor('grey')
@@ -19,29 +16,41 @@ fig.set_facecolor('grey')
 year = [1950, 1951, 1952, 1953, 1954, 1955, 1956, 1957, 1958, 1959, 1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034, 2035, 2036, 2037, 2038, 2039, 2040, 2041, 2042, 2043, 2044, 2045, 2046, 2047, 2048, 2049, 2050, 2051, 2052, 2053, 2054, 2055, 2056, 2057, 2058, 2059, 2060, 2061, 2062, 2063, 2064, 2065, 2066, 2067, 2068, 2069, 2070, 2071, 2072, 2073, 2074, 2075, 2076, 2077, 2078, 2079, 2080, 2081, 2082, 2083, 2084, 2085, 2086, 2087, 2088, 2089, 2090, 2091, 2092, 2093, 2094, 2095, 2096, 2097, 2098, 2099, 2100]
 pop = [2.53, 2.57, 2.62, 2.67, 2.71, 2.76, 2.81, 2.86, 2.92, 2.97, 3.03, 3.08, 3.14, 3.2, 3.26, 3.33, 3.4, 3.47, 3.54, 3.62, 3.69, 3.77, 3.84, 3.92, 4.0, 4.07, 4.15, 4.22, 4.3, 4.37, 4.45, 4.53, 4.61, 4.69, 4.78, 4.86, 4.95, 5.05, 5.14, 5.23, 5.32, 5.41, 5.49, 5.58, 5.66, 5.74, 5.82, 5.9, 5.98, 6.05, 6.13, 6.2, 6.28, 6.36, 6.44, 6.51, 6.59, 6.67, 6.75, 6.83, 6.92, 7.0, 7.08, 7.16, 7.24, 7.32, 7.4, 7.48, 7.56, 7.64, 7.72, 7.79, 7.87, 7.94, 8.01, 8.08, 8.15, 8.22, 8.29, 8.36, 8.42, 8.49, 8.56, 8.62, 8.68, 8.74, 8.8, 8.86, 8.92, 8.98, 9.04, 9.09, 9.15, 9.2, 9.26, 9.31, 9.36, 9.41, 9.46, 9.5, 9.55, 9.6, 9.64, 9.68, 9.73, 9.77, 9.81, 9.85, 9.88, 9.92, 9.96, 9.99, 10.03, 10.06, 10.09, 10.13, 10.16, 10.19, 10.22, 10.25, 10.28, 10.31, 10.33, 10.36, 10.38, 10.41, 10.43, 10.46, 10.48, 10.5, 10.52, 10.55, 10.57, 10.59, 10.61, 10.63, 10.65, 10.66, 10.68, 10.7, 10.72, 10.73, 10.75, 10.77, 10.78, 10.79, 10.81, 10.82, 10.83, 10.84, 10.85]
 
-
 print(f'Last year with estimation: {year[-1]}')
 print(f'Last estimated population: {pop[-1]}')
 # print(len(year))          # same len, will just use the index of pop > 10
 # print(len(pop))
-idx_pop_geq_10 = next(x for x, val in enumerate(pop) if val > 10)   # https://www.tutorialspoint.com/python-get-the-index-of-first-element-greater-than-k
+idx_pop_geq_10 = next(
+    x for x, val in enumerate(pop) if val > 10
+)  # https://www.tutorialspoint.com/python-get-the-index-of-first-element-greater-than-k
 
 ax1.set_facecolor('grey')
-ax1.plot(year,pop, color='gold')
+ax1.plot(year, pop, color='gold')
 ax1.set_xlabel('Year')
 ax1.set_ylabel('Population')
 # plt.tick_params(color='gray')
 ax1.set_title('World Population Projections')
-ax1.axhline(y=pop[idx_pop_geq_10], color="black", ls="--", lw=0.5, label='>10b')       # modified from gpt, would be prettier if not full width/height 
-ax1.axvline(x=year[idx_pop_geq_10], color="black", ls="--", lw=0.5, label=year[idx_pop_geq_10])
+ax1.axhline(y=pop[idx_pop_geq_10],
+            color="black",
+            ls="--",
+            lw=0.5,
+            label='>10b'
+            )  # modified from gpt, would be prettier if not full width/height
+ax1.axvline(x=year[idx_pop_geq_10],
+            color="black",
+            ls="--",
+            lw=0.5,
+            label=year[idx_pop_geq_10])
 
-ax1.annotate(f'{pop[idx_pop_geq_10]}b / ', 
+ax1.annotate(f'{pop[idx_pop_geq_10]}b / ',
              xy=(year[idx_pop_geq_10], pop[idx_pop_geq_10]),
-             ha="right", va="bottom")
+             ha="right",
+             va="bottom")
 
-ax1.annotate(year[idx_pop_geq_10], 
+ax1.annotate(year[idx_pop_geq_10],
              xy=(year[idx_pop_geq_10], pop[idx_pop_geq_10]),
-             ha="left", va="bottom")
+             ha="left",
+             va="bottom")
 
 # life expect over gdp/cap
 life_exp = [43.828, 76.423, 72.301, 42.731, 75.32, 81.235, 79.829, 75.635, 64.062, 79.441, 56.728, 65.554, 74.852, 50.728, 72.39, 73.005, 52.295, 49.58, 59.723, 50.43, 80.653, 44.74100000000001, 50.651, 78.553, 72.961, 72.889, 65.152, 46.462, 55.322, 78.782, 48.328, 75.748, 78.273, 76.486, 78.332, 54.791, 72.235, 74.994, 71.33800000000001, 71.878, 51.57899999999999, 58.04, 52.947, 79.313, 80.657, 56.735, 59.448, 79.406, 60.022, 79.483, 70.259, 56.007, 46.388000000000005, 60.916, 70.19800000000001, 82.208, 73.33800000000001, 81.757, 64.69800000000001, 70.65, 70.964, 59.545, 78.885, 80.745, 80.546, 72.567, 82.603, 72.535, 54.11, 67.297, 78.623, 77.58800000000001, 71.993, 42.592, 45.678, 73.952, 59.443000000000005, 48.303, 74.241, 54.467, 64.164, 72.801, 76.195, 66.803, 74.543, 71.164, 42.082, 62.069, 52.906000000000006, 63.785, 79.762, 80.204, 72.899, 56.867, 46.859, 80.196, 75.64, 65.483, 75.53699999999999, 71.752, 71.421, 71.688, 75.563, 78.098, 78.74600000000001, 76.442, 72.476, 46.242, 65.528, 72.777, 63.062, 74.002, 42.568000000000005, 79.972, 74.663, 77.926, 48.159, 49.339, 80.941, 72.396, 58.556, 39.613, 80.884, 81.70100000000001, 74.143, 78.4, 52.517, 70.616, 58.42, 69.819, 73.923, 71.777, 51.542, 79.425, 78.242, 76.384, 73.747, 74.249, 73.422, 62.698, 42.38399999999999, 43.487]
@@ -49,14 +58,16 @@ gdp_cap = [974.5803384, 5937.029525999999, 6223.367465, 4797.231267, 12779.37964
 
 ax2.set_facecolor('grey')
 ax2.set_xscale('log')
-ax2.scatter(gdp_cap, life_exp, color='gold')  # might be better in log, upper gdp vals are orders of magnitute larger than begining vals
+ax2.scatter(
+    gdp_cap, life_exp, color='gold'
+)  # might be better in log, upper gdp vals are orders of magnitute larger than begining vals
 
 ax2.set_xlabel('GDP per capita')
 ax2.set_ylabel('Life expectancy')
 ax2.set_title('Life expectancy against GDP per capita')
 
-ticks = [500, 1000, 2000, 5000, 10000, 20000, 50000]                # GPT
-ax2.set_xticks(ticks, labels=['500','1k','2k','5k','10k','20k','50k'])
+ticks = [500, 1000, 2000, 5000, 10000, 20000, 50000]  # GPT
+ax2.set_xticks(ticks, labels=['500', '1k', '2k', '5k', '10k', '20k', '50k'])
 
 plt.tight_layout()
 plt.show()
