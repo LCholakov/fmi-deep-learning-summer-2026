13f1af86bac1f36f3ab11f80bad66136cbd8d2e2
Lyubomir Cholakov <39132342+LCholakov@users.noreply.github.com>
2026-03-08 18:10:16 +0200

    w01 build perceptrons task01 answer question

--- week_01/engineering/building_perceptrons/task01.py
+++ week_01/engineering/building_perceptrons/task01.py
@@ -15,5 +15,15 @@ def main():
     print(initialize_weights(0,100))
     print(initialize_weights(0,10))
 
+# Q: A general form of the model is placed in a comment that shows how many parameters the model has.
+# A: Each training sample has one input and one expected output.
+# The model will have only one parameter (weight w). Weight will modify the input. 
+# We'll then compare the output to the expected result and calculate how to change 
+# the weight. 
+#
+# ※ Note on bias - ignoring bias for this model, but the general model would require
+# a bias as well. This would bring the parameter count up to 2, but in this 
+# case I think bias would be set to zero, so I'm just skipping it. 
+
 if __name__ == "__main__":
     main()
